ae9e9f8505f651f12f8bd8079fb2531d306c3974
ahimmelstoss <ahimmelstoss@gmail.com>
2014-05-14 12:59:02 -0400

    testing for master branch

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -23,7 +23,7 @@ describe PostsController do
   # This should return the minimal set of attributes required to create a valid
   # Post. As you add validations to Post, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "name" => "MyString" } }
+  let(:valid_attributes) { { "name" => "MyString", "content" => "content here" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  its "post is valid" do 
+    expect(FactoryGirl.create(:cheeseburger_post)).to be_valid
+  end
+
+  its "invalid with no name" do 
+    expect(FactoryGirl.build(:invalid_post_name)).to_not be_valid
+  end
+
+  its "invalid with no content" do 
+    expect(FactoryGirl.build(:invalid_post_content)).to_not be_valid
+  end
+
+  subject(:post_associated_with_one_tag) {FactoryGirl.create(:harry_potter_post)}
+  its "able to have one tag association" do
+    expect(post_associated_with_one_tag.tags.length).to eq(1)
+  end
+
+  subject(:post_associated_with_tags) {FactoryGirl.create(:kitten_post)}
+  its "able to have multiple tags" do
+    expect(post_associated_with_tags.tags.length).to eq(3)
+  end
+
 end
