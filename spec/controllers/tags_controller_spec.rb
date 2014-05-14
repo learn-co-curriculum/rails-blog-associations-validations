5b61ff85ff5c8f410b81509b2a23704c1bcb5a2e
ahimmelstoss <ahimmelstoss@gmail.com>
2014-05-14 12:44:35 -0400

    fixing controller specs

--- spec/controllers/tags_controller_spec.rb
+++ spec/controllers/tags_controller_spec.rb
@@ -23,7 +23,7 @@ describe TagsController do
   # This should return the minimal set of attributes required to create a valid
   # Tag. As you add validations to Tag, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { {  } }
+  let(:valid_attributes) { { "name" => "I'm a tag!" } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
