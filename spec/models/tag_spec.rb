b6b43fce191e51e710f9ecfdbfc9f40d1be464d4
Amanda Himmelstoss <ahimmelstoss@gmail.com>
2014-06-05 11:31:18 -0400

    Update tag_spec.rb

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -6,10 +6,6 @@ describe Tag do
       expect(Tag.create(name: "happy")).to be_valid
     end
 
-    its "invalid with no name" do 
-      expect(Tag.create(name: nil)).to_not be_valid
-    end
-
     its "invalid if name already exists" do
       Tag.create(name: "happy")
       expect(Tag.create(name: "happy")).to_not be_valid
