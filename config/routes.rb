8c00932f6affb2a5323fa6cbdfed1325eb7f0c81
ahimmelstoss <ahimmelstoss@gmail.com>
2014-09-22 13:44:14 -0400

    add solution

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+RailsBlog::Application.routes.draw do
+
+  resources :users
+  resources :tags
+  resources :posts
+
 end
--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -2,8 +2,9 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :name
-
-      t.timestamps null: false
+      t.text :content
+      t.integer :user_id
+      t.timestamps
     end
   end
 end
