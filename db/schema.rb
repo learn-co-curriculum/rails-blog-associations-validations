fc4f6f596f9b8facef04a013224effdd4e798c96
maxwellbenton <maxwellbenton@gmail.com>
2019-02-14 16:20:43 -0500

    add schema to solution

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -24,8 +23,8 @@ ActiveRecord::Schema.define(version: 20140513204421) do
     t.string   "name"
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "tags", force: :cascade do |t|
